3f3cd7ba18741d7c554ae505c23b5951299c77ed
Mason DiGiorgio <mason@digiorgiofamily.net>
2022-03-21 08:50:37 -0400

    Added class to manage photonvision through networktables

---
+++ src/main/java/frc/robot/PhotonCamera.java
@@ -0,0 +1,16 @@
+package frc.robot;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+public class PhotonCamera {
+    public static NetworkTable driverCam = NetworkTableInstance.getDefault().getTable("photonvision").getSubTable("HD_USB_Camera");
+
+    public static double getArea() {
+        return driverCam.getEntry("targetArea").getDouble(0);
+    }
+
+    public static double getYaw() {
+        return driverCam.getEntry("targetYaw").getDouble(0);
+    }
+}
